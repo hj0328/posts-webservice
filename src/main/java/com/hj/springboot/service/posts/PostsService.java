1b4a5047cad806f855d5a65fc0c5ede108f6735c
hojun <hjlee9303@gmail.com>
2025-01-07 21:19:06 +0900

    [Feat] 게시글 조회 및 등록 - 홈화면에서 보여줄 게시글 조회 기능 추가

--- src/main/java/com/hj/springboot/service/posts/PostsService.java
+++ src/main/java/com/hj/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.hj.springboot.service.posts;
 
 import com.hj.springboot.web.domain.posts.Posts;
 import com.hj.springboot.web.domain.posts.PostsRepository;
+import com.hj.springboot.web.dto.PostsListResponseDto;
 import com.hj.springboot.web.dto.PostsResponseDto;
 import com.hj.springboot.web.dto.PostsSaveRequestDto;
 import com.hj.springboot.web.dto.PostsUpdateRequestDto;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
