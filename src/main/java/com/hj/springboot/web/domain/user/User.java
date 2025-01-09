a2afae0667ab424c5c2af46bd8c3954414fb6636
hojun <hjlee9303@gmail.com>
2025-01-10 00:00:10 +0900

    [Feat] User 도메인 추가 - User Entity 추가 - User Entity에 Role ENUM 클래스 추가   데이터베이스에서도 해당 타입만 저장가능

---
+++ src/main/java/com/hj/springboot/web/domain/user/User.java
@@ -0,0 +1,50 @@
+package com.hj.springboot.web.domain.user;
+
+import com.hj.springboot.web.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "APP_USER")
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Column
+    @Enumerated(EnumType.STRING)    // Enum 타입 DB 저장 시의 타입 설정
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
+}
