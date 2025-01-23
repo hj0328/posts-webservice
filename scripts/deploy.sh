44f63d7de0fc298fb6d3295f48a163e66f74181a
hojun <hjlee9303@gmail.com>
2025-01-23 16:31:35 +0900

    [FIX] 배포 자동화 deploy.sh 수정 - 배포 후 재기동 시 애플리케이션 pid 정상적으로 조회하도록 수정 - 어플리케이션 이름이 아닌 포트로 pid 조회

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,16 +9,23 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl posts-webservice | grep jar | awk '{print $fl')
+CURRENT_PID=$(lsof -ti :8080)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID"]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동 중인 애플리케이션이 없습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> 현재 구동 중인 애플리케이션 PID: $CURRENT_PID"
+  echo "> 프로세스 종료 시도: kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
+
+  # 프로세스가 종료되지 않은 경우 강제 종료
+  if ps -p $CURRENT_PID > /dev/null; then
+    echo "> 프로세스가 종료되지 않아 강제 종료합니다: kill -9 $CURRENT_PID"
+    kill -9 $CURRENT_PID
+  fi
 fi
 
 echo "> 새 애플리케이션 배포"
@@ -34,8 +41,10 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-      -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,\
-      classpath:/home/ec2-user/app/application-oauth.properties,\
-      /home/ec2-user/app/application-real-db.properties\
-      -Dspring.profiles.active=real \
-      $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,\
+file:/home/ec2-user/app/application-oauth.properties,\
+file:/home/ec2-user/app/application-real-db.properties \
+  -Dspring.profiles.active=real \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+
